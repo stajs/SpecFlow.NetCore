d0936d131dbb64a45609c7ffe218636d9bbe7bc1
Erik Lundgren <erik.lundgren@hiq.se>
2016-08-09 18:50:12 +0200

    Added support for ASP.NET Core 1.0.0

---
+++ samples/VS2015/SpecFlow 1.9.0/net461 1.0.0/Sample.Website/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Sample.Website.Controllers
+{
+	public class HomeController : Controller
+	{
+		public const string Dnx = "dnx46 1.0.0-rc1-final";
+
+		public IActionResult Index()
+		{
+			return View((object)Dnx);
+		}
+
+		public IActionResult Version()
+		{
+			return Content(Dnx);
+		}
+
+		public IActionResult Echo(string s)
+		{
+			return Content(s);
+		}
+	}
+}
