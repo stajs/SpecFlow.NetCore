3fa97e3dc90422f8530a37b6d607a58e7dc5588e
stajs <shannonsnz@gmail.com>
2016-08-30 20:39:01 +1200

    Drop support for SpecFlow versions below v2.1.0.

--- src/SpecFlow.NetCore/Fixer.cs
+++ src/SpecFlow.NetCore/Fixer.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using System.Xml.XPath;
+using static System.Console;
 
 namespace SpecFlow.NetCore
 {
 	internal class Fixer
 	{
-		public readonly string SpecFlowExe;
-
 		private const string AppConfigSpecFlowSectionDefinitionType = "TechTalk.SpecFlow.Configuration.ConfigurationSectionHandler, TechTalk.SpecFlow";
 
 		private const string AppConfigSpecFlowSectionDefinition = @"	<configSections>
@@ -26,53 +24,25 @@ namespace SpecFlow.NetCore
 		<" + AppConfigSpecFlowSectionElement + @" name=""xUnit"" />
 	</specFlow>";
 
+		private readonly string _specFlowExe;
+
 		public Fixer()
 		{
-			// TODO: Allow for other DNX install locations.
-			var users = Directory.GetDirectories(@"C:\Users");
+			// For full .NET Framework, you can get the user profile with: Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+			// This isn't available yet in .NET Core, so rely on the environment variable for now.
+			var userProfile = Environment.GetEnvironmentVariable("USERPROFILE");
 
-			var dnxUser = users.SingleOrDefault(u =>
-			{
-				try
-				{
-					return Directory.GetDirectories(u).Any(d => d.Contains(".dnx"));
-				}
-				catch (UnauthorizedAccessException)
-				{
-					return false;
-				}
-			});
-			if (dnxUser == null)
-			{
-				throw new Exception("Can't find a user with a .dnx folder in his/her home directory");
-			}
+			_specFlowExe = Path.Combine(userProfile, @".nuget\packages\SpecFlow\2.1.0\tools\specflow.exe");
 
-			var SpecFlowPaths = new List<string>
-				{
-					 Path.Combine(dnxUser, @".nuget\packages\SpecFlow\2.1.0\tools\specflow.exe"),
-					 Path.Combine(dnxUser, @".dnx\packages\SpecFlow\2.1.0\tools\specflow.exe"),
-					 Path.Combine(dnxUser, @".nuget\packages\SpecFlow\2.0.0\tools\specflow.exe"),
-					 Path.Combine(dnxUser, @".dnx\packages\SpecFlow\2.0.0\tools\specflow.exe"),
-					 Path.Combine(dnxUser, @".nuget\packages\SpecFlow\1.9.0\tools\specflow.exe"),
-					 Path.Combine(dnxUser, @".dnx\packages\SpecFlow\1.9.0\tools\specflow.exe")
-				};
+			if (!File.Exists(_specFlowExe))
+				throw new Exception("Can't find SpecFlow: " + _specFlowExe);
 
-			for (var i = 0; i < SpecFlowPaths.Count && !File.Exists(SpecFlowExe); i++)
-			{
-				SpecFlowExe = SpecFlowPaths[i];
-			}
-
-			if (!File.Exists(SpecFlowExe))
-			{
-				throw new Exception("Can't find SpecFlow: " + SpecFlowExe);
-			}
-
-			Console.WriteLine("Found: " + SpecFlowExe);
+			WriteLine("Found: " + _specFlowExe);
 		}
 
 		public void Fix(DirectoryInfo directory)
 		{
-			Console.WriteLine("Current directory: " + directory);
+			WriteLine("Current directory: " + directory);
 			var xproj = GetXproj(directory);
 			var fakeCsproj = SaveFakeCsProj(directory, xproj);
 			GenerateSpecFlowGlue(directory, fakeCsproj);
@@ -82,19 +52,19 @@ namespace SpecFlow.NetCore
 
 		private void DeleteFakeCsProj(FileInfo fakeCsproj)
 		{
-			Console.WriteLine("Removing: " + fakeCsproj.FullName);
+			WriteLine("Removing: " + fakeCsproj.FullName);
 			fakeCsproj.Delete();
 		}
 
 		private void FixXunit(DirectoryInfo directory)
 		{
-			Console.WriteLine("Fixing SpecFlow generated files for xUnit 2");
+			WriteLine("Fixing SpecFlow generated files for xUnit 2");
 
 			var glueFiles = directory.GetFiles("*.feature.cs", SearchOption.AllDirectories);
 
 			foreach (var glueFile in glueFiles)
 			{
-				Console.WriteLine("Fixed: " + glueFile.FullName);
+				WriteLine("Fixed: " + glueFile.FullName);
 				var content = File.ReadAllText(glueFile.FullName);
 				content = content.Replace(" : Xunit.IUseFixture<", " : Xunit.IClassFixture<");
 				content = content.Replace("[Xunit.Extensions", "[Xunit");
@@ -107,13 +77,13 @@ namespace SpecFlow.NetCore
 			// Target later version of .NET.
 			// Credit: http://stackoverflow.com/questions/11363202/specflow-fails-when-trying-to-generate-test-execution-report
 
-			Console.WriteLine("Generating specflow.exe.config.");
+			WriteLine("Generating specflow.exe.config.");
 
-			var configPath = SpecFlowExe + ".config";
+			var configPath = _specFlowExe + ".config";
 			var content = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><configuration><startup><supportedRuntime version=\"v4.0.30319\" /></startup></configuration>";
-			Console.WriteLine(content);
+			WriteLine(content);
 
-			Console.WriteLine("Saving: " + configPath);
+			WriteLine("Saving: " + configPath);
 			File.WriteAllText(configPath, content);
 
 			return configPath;
@@ -126,7 +96,7 @@ namespace SpecFlow.NetCore
 			if (File.Exists(path))
 				return path;
 
-			Console.WriteLine("Generating app.config.");
+			WriteLine("Generating app.config.");
 
 			var content = $@"<?xml version=""1.0"" encoding=""utf-8""?>
 <configuration>
@@ -134,9 +104,9 @@ namespace SpecFlow.NetCore
 {AppConfigSpecFlowSection}
 </configuration>";
 
-			Console.WriteLine(content);
+			WriteLine(content);
 
-			Console.WriteLine("Saving: " + path);
+			WriteLine("Saving: " + path);
 			File.WriteAllText(path, content);
 
 			return path;
@@ -144,7 +114,7 @@ namespace SpecFlow.NetCore
 
 		private void ValidateAppConfig(string path)
 		{
-			Console.WriteLine("Validating app.config.");
+			WriteLine("Validating app.config.");
 
 			// I would rather use the ConfigurationBuilder, but as of beta8 it fails to read an element without
 			// a value, e.g.: <unitTestProvider name="xUnit" />
@@ -169,7 +139,7 @@ namespace SpecFlow.NetCore
 		{
 			// Credit: http://www.marcusoft.net/2010/12/specflowexe-and-mstest.html
 			var arguments = $"generateall {csproj} /force /verbose";
-			Console.WriteLine($"Calling: {SpecFlowExe} {arguments}");
+			WriteLine($"Calling: {_specFlowExe} {arguments}");
 
 			var p = new Process
 			{
@@ -177,7 +147,7 @@ namespace SpecFlow.NetCore
 					 {
 						  UseShellExecute = false,
 						  RedirectStandardOutput = true,
-						  FileName = SpecFlowExe,
+						  FileName = _specFlowExe,
 						  Arguments = arguments
 					 }
 			};
@@ -187,7 +157,7 @@ namespace SpecFlow.NetCore
 			var output = p.StandardOutput.ReadToEnd();
 			p.WaitForExit();
 
-			Console.WriteLine(output);
+			WriteLine(output);
 
 			if (output.Contains("-> test generation failed"))
 				throw new Exception("SpecFlow generation failed (review the output).");
@@ -195,7 +165,7 @@ namespace SpecFlow.NetCore
 
 		private void DeleteSpecFlowConfig(string configPath)
 		{
-			Console.WriteLine("Removing the SpecFlow config file.");
+			WriteLine("Removing the SpecFlow config file.");
 			File.Delete(configPath);
 		}
 
@@ -210,7 +180,7 @@ namespace SpecFlow.NetCore
 
 		private FileInfo SaveFakeCsProj(DirectoryInfo directory, FileInfo xproj)
 		{
-			Console.WriteLine("Generating fake csproj.");
+			WriteLine("Generating fake csproj.");
 
 			var featureFiles = directory.GetFiles("*.feature", SearchOption.AllDirectories);
 			var sb = new StringBuilder();
@@ -241,10 +211,10 @@ namespace SpecFlow.NetCore
 </Project>");
 
 			var content = sb.ToString();
-			Console.WriteLine(content);
+			WriteLine(content);
 
 			var csprojPath = xproj.FullName + ".fake.csproj";
-			Console.WriteLine("Saving: " + csprojPath);
+			WriteLine("Saving: " + csprojPath);
 			File.WriteAllText(csprojPath, content);
 
 			return new FileInfo(csprojPath);
@@ -261,7 +231,7 @@ namespace SpecFlow.NetCore
 				throw new Exception("More than one '.xproj' found.");
 
 			var xproj = xprojs.Single();
-			Console.WriteLine("Found: " + xproj.FullName);
+			WriteLine("Found: " + xproj.FullName);
 
 			return xproj;
 		}
