1785d5594ad9fc66735e26a641340afb66690cf1
Erik Lundgren <erik.lundgren1@gmail.com>
2016-10-03 02:49:04 +0200

    Scenario Outline tests are correctly categorized (#36)
    They will be categorized under the project the feature file belongs to
    instead of being categorized under External.
    
    See https://github.com/techtalk/SpecFlow/issues/275.
    
    Fixes #34

--- src/SpecFlow.NetCore/AppConfig.cs
+++ src/SpecFlow.NetCore/AppConfig.cs
@@ -52,6 +52,14 @@ namespace SpecFlow.NetCore
 				return config;
 
 			Content = string.Format(Content, testFramework);
+
+			// fixes SpecFlow Scenario Outline scenarios appearing under the project they belong to.
+			// see https://github.com/stajs/SpecFlow.NetCore/issues/34 and https://github.com/techtalk/SpecFlow/issues/275
+			if (testFramework.ToLower() == "mstest")
+			{
+				Content = Content.Replace("</specFlow>", "  <generator allowDebugGeneratedFiles=\"true\" />\r\n  </specFlow>");
+			}
+
 			WriteLine("Generating app.config");
 			WriteLine(Content);
 			WriteLine("Saving: " + config.Path);
